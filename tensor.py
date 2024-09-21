01e172b1c589a79fa3e6da11f58025c7e102fb4a
finn <112250336+finn200102@users.noreply.github.com>
2024-09-21 15:50:09 +0200

    added gradient for addition

--- tensor.py
+++ tensor.py
@@ -17,3 +17,25 @@ class Tensor:
                    "operation": Operations.plus}
         return Tensor(self.value + other.value, history=history)
 
+    def differentiation(self):
+        if self.history["operation"] == Operations.plus:
+            return {"diff1": np.eye(len(self.history["value1"].value)),
+                    "diff2": np.eye(len(self.history["value2"].value))}
+
+    def gradient(self, root):
+        if self.history:
+            differentiation = self.differentiation()
+            if "value1" in self.history:
+                if self == root:
+                    grad1 = differentiation["diff1"] @ np.ones_like(self.grads)
+                else:
+                    grad1 = differentiation["diff1"] @ self.grads
+                self.history["value1"].grads += grad1
+            if "value" in self.history:
+                if self == root:
+                    grad2 = differentiation["diff2"] @ np.ones_like(self.grads)
+                else:
+                    grad2 = differentiation["diff2"] @ self.grads
+                self.history["value2"].grads += grad2
+
+
