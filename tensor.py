4466968618dc91f30e3fffd5dfe31f68452051af
finn <112250336+finn200102@users.noreply.github.com>
2024-09-21 18:12:16 +0200

    added matrixmul + backprop working for simple test

--- tensor.py
+++ tensor.py
@@ -4,6 +4,8 @@ from enum import Enum
 
 class Operations(Enum):
     plus = "+"
+    matmul = "@"
+    mul = "*"
 
 class Tensor:
     def __init__(self, value, history={}):
@@ -17,25 +19,73 @@ class Tensor:
                    "operation": Operations.plus}
         return Tensor(self.value + other.value, history=history)
 
+    def __mul__(self, other):
+        # elementwise multiplication
+        history = {"value1": self,
+                   "value2": other,
+                   "operation": Operations.mul}
+        return Tensor(self.value * other.value, history=history)
+
+    def __matmul__(self, other):
+        history = {"value1": self,
+                   "value2": other,
+                   "operation": Operations.matmul}
+        return Tensor(self.value @ other.value, history=history)
+
     def differentiation(self):
         if self.history["operation"] == Operations.plus:
-            return {"diff1": np.eye(len(self.history["value1"].value)),
-                    "diff2": np.eye(len(self.history["value2"].value))}
+            return {"diff1": np.eye(len(np.atleast_1d(
+                self.history["value1"].value))),
+                    "diff2": np.eye(len(np.atleast_1d(
+                        self.history["value2"].value)))}
+
+        if self.history["operation"] == Operations.mul:
+            return {"diff1": self.history["value2"].value,
+                    "diff2": self.history["value1"].value}
+
+        if self.history["operation"] == Operations.matmul:
+            # case 1: value1 is matrix and value2 is a vector
+            A = self.history["value1"].value
+            x = self.history["value2"].value
+            m, n = A.shape
+    
+            # Create the 3D tensor for dy/dA
+            dy_dA = np.zeros((m, n, m))
+            for i in range(m):
+                dy_dA[i, :, i] = x.flatten()
+
+            return {"diff1": dy_dA, 
+                    "diff2": A}
+
+
+    def scalar_or_matmul(self, a, b):
+        if np.isscalar(a) or np.isscalar(b) or a.shape == () or b.shape == ():
+            print("scalar")
+            print(a)
+            print(b)
+            return a * b  # Scalar product
+        else:
+            print("matmul")
+            print(a)
+            print(b)
+            return a @ b  # Matrix multiplication
 
     def gradient(self, root):
         if self.history:
             differentiation = self.differentiation()
             if "value1" in self.history:
                 if self == root:
-                    grad1 = differentiation["diff1"] @ np.ones_like(self.grads)
+                    grad1 = self.scalar_or_matmul(differentiation["diff1"], np.ones_like(self.grads))
                 else:
-                    grad1 = differentiation["diff1"] @ self.grads
-                self.history["value1"].grads += grad1
-            if "value" in self.history:
+                    grad1 = self.scalar_or_matmul(differentiation["diff1"], self.grads)
+                print("---")
+                print(grad1) 
+                self.history["value1"].grads += np.reshape(grad1, self.history["value1"].grads.shape)
+            if "value2" in self.history:
                 if self == root:
-                    grad2 = differentiation["diff2"] @ np.ones_like(self.grads)
+                    grad2 = self.scalar_or_matmul(differentiation["diff2"], np.ones_like(self.grads))
                 else:
-                    grad2 = differentiation["diff2"] @ self.grads
-                self.history["value2"].grads += grad2
+                    grad2 = self.scalar_or_matmul(differentiation["diff2"], self.grads)
+                self.history["value2"].grads += np.reshape(grad2, self.history["value2"].grads.shape)
 
 
---
+++ test_tensor.py
@@ -0,0 +1,53 @@
+from tensor import Tensor
+import numpy as np
+
+
+def test_addition():
+    a = Tensor(np.array([1, 2]))
+    b = Tensor(np.array([3, 4]))
+    c = a + b
+    c.gradient(c)
+    assert np.allclose(a.grads, np.array([1, 1], dtype=np.float64)), f"""
+    The gradient {a.grads} is not {np.array([1, 1], dtype=np.float64)}"""
+
+    A = Tensor(np.array([[1, 2], [3, 4]]))
+    B = Tensor(np.array([[1, 2], [3, 4]]))
+    C = A + B
+    C.gradient(C)
+    assert np.allclose(A.grads, np.array([[1, 1], [1, 1]], 
+                                         dtype=np.float64)), f"""
+    The gradient {A.grads} is not {np.array([[1, 1], [1, 1]], 
+                                         dtype=np.float64)}"""
+
+    e = Tensor(np.array(1))
+    f = Tensor(np.array(1))
+    g = e + f
+    g.gradient(g)
+    assert np.allclose(a.grads, np.array(1, dtype=np.float64)), f"""
+    The gradient {a.grads} is not {np.array(1, dtype=np.float64)}"""
+
+
+def test_matmul():
+    A = Tensor(np.array([[1, 2], [3, 4]]))
+    x = Tensor(np.array([1, 2]))
+    y = A @ x
+    y.gradient(y)
+    assert np.allclose(A.grads, np.array([[1, 2], [1, 2]], 
+                                         dtype=np.float64)), f"""
+    The gradient {A.grads} is not {np.array([[1, 2], [1, 2]], dtype=np.float64)}"""
+
+    assert np.allclose(x.grads, np.array([3, 7], 
+                                         dtype=np.float64)), f"""
+    The gradient {x.grads} is not {np.array([3, 7], dtype=np.float64)}"""
+
+def test_mul():
+    a = Tensor(np.array([1, 2]))
+    b = Tensor(2)
+    c = a * b
+    c.gradient(c)
+    print(a.grads)
+    print(b.grads)
+
+#test_addition()
+#test_matmul()
+test_mul()
